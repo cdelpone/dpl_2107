a8e3ff8ff0635f64af80c5a5100a68ccf32f481c
Christina Delpone <cdelpone@gmail.com>
2021-07-29 13:54:11 -0600

    Compelte iteration1

---
+++ lib/book.rb
@@ -0,0 +1,18 @@
+class Book
+attr_reader :author_first_name,
+            :author_last_name,
+            :title,
+            :publication_date,
+            :author
+  def initialize(info)
+    @author_first_name = info[:author_first_name]
+    @author_last_name = info[:author_last_name]
+    @title = info[:title]
+    @publication_date = info[:publication_date]
+    @author = @author_first_name + ' ' + @author_last_name
+  end
+
+  def publication_year
+    @publication_date.chars.last(4).join
+  end
+end
